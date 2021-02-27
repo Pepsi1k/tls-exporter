55b7c02c70a062512faa62390c822ecfe7a8b1f8
Pepsi1k <megasharff@gmail.com>
2021-02-27 16:46:13 +0200

    add metrics

--- main.go
+++ main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
-  "github.com/prometheus/client_golang/prometheus"
-  "github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-
 func recordMetrics() {
-  go func() {
-    for {
-      opsProcessed.Inc()
-      time.Sleep
-    }
-  }
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
 }
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "cert",
+		Help: "describe info about certificate",
+	})
+)
 
 func main() {
 
